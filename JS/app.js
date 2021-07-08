c5f86c22cb32a8ae930272d4036da95fd73b6048
Ar on <aronbjarklind@gmail.com>
2021-07-08 22:01:24 +0000

    þrjár myndir og flex responsive

---
+++ JS/app.js
@@ -0,0 +1,38 @@
+/*jshint esversion:6 */
+
+
+const baby = document.querySelectorAll("#breytaLitTexta");
+console.log(baby);
+
+const divyMinn = document.querySelector(".parent");
+
+svartur.addEventListener("click", svartur1);
+hvitur.addEventListener("click", hvitur2);
+gulur.addEventListener("click", gulur3);
+
+
+function svartur1() {
+    
+    divyMinn.style.color = "black";
+}
+
+function hvitur2() {
+    
+    divyMinn.style.color = "white";
+}
+
+function gulur3() {
+    
+    divyMinn.style.color = "yellow";
+}
+
+console.log(divyMinn);
+
+
+
+
+
+// function james() {
+    
+//    divyMinn.style.color = "black";
+// }
